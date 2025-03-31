1ac171f444f2ef566b89d2c50019756cb7c2a9ae
DavidCaba6194 <cabayosoyasi@gmail.com>
2025-03-31 22:03:52 +0200

    Corrección para dark mode

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,16 +1,17 @@
 <template>
-  <div :class="{'full-menu-container': true, 'expanded': isMenuShow || isSearchShow}">
+  <div :class="{'full-menu-container': true, 'expanded': isMenuShow}">
     <div class="menu-container">
       <div class="hamburger-menu" @click="toggleMenu()">
         <div class="bar" ref="bar"></div>
       </div>
       <div class="logo-container">
         <NuxtLink to="/">
-          <img src="../assets/img/icono.png" alt="Logo" style="width: 50px; height: 50px;" />
+          <img src="../assets/img/icono.png" alt="Logo" class="img-logo" />
         </NuxtLink>
       </div>
-      <div :class="isSearchShow ? 'search-icon search-active' : 'search-icon'" @click="toggleSearch()">
-        <Icon name="uil:search"/>
+      <div class="night-icon" @click="toggleDarkMode()">
+        <Icon v-if="!isDarkMode" name="uil:moon"/>
+        <Icon v-else name="uil:sun"/>
       </div>
     </div>
     <transition name="dropdown">
@@ -22,32 +23,16 @@
         <NuxtLink to="/videos" :class="activeRoute === '/videos' ? 'active' : ''">Vídeos</NuxtLink>
       </div>
     </transition>
-    <transition name="dropdown">
-      <div v-if="isSearchShow" class="search-container">
-        <input v-model="searchText" type="text" placeholder="Buscar..." @keyup="searchItems()" />
-        <div v-if="searchText !== ''">
-          <div class="results-container">
-            <NuxtLink v-for="item in searchedItems" :key="item.id" :to="item.link" class="result-item">
-              <div class="result-item-img"></div>
-              <div class="result-item-name">{{ item.name }}</div>
-            </NuxtLink>
-          </div>
-        </div>
-        <div class="no-items-text" v-if="searchText !== '' && searchedItems.length === 0">No hay resultados disponibles</div>
-      </div>
-    </transition>
   </div>
 </template>
 
 <script>
-import '~/assets/css/menu.css'
-
 export default {
   name: 'Menu',
   data() {
     return {
       isMenuShow: false,
-      isSearchShow: false,
+      isDarkMode: false,
       searchText: '',
       searchedItems: [],
       items: [
@@ -66,13 +51,11 @@ export default {
   },
   methods: {
     toggleMenu() {
-      if (this.isSearchShow) this.toggleSearch();
       this.$refs.bar.classList.toggle('animate');
       this.isMenuShow = !this.isMenuShow;
     },
-    toggleSearch() {
-      if (this.isMenuShow) this.toggleMenu();
-      this.isSearchShow = !this.isSearchShow;
+    toggleDarkMode() {
+      this.isDarkMode = !this.isDarkMode;
     },
     searchItems() {
       this.searchedItems = this.searchText
@@ -99,12 +82,15 @@ export default {
   max-height: 50px;
   overflow: hidden;
 }
+
 .full-menu-container.expanded {
   max-height: 300px;
 }
+
 .dropdown-enter-active, .dropdown-leave-active {
   transition: opacity 0.5s ease, transform 0.5s ease;
 }
+
 .dropdown-enter-from, .dropdown-leave-to {
   opacity: 0;
   transform: translateY(-20px);
@@ -127,23 +113,21 @@ export default {
   flex-wrap: nowrap;
 }
 
-.logo-title {
-  font-size: 25px;
-  margin-left: 20px;
+.img-logo {
+  width: 50px;
+  height: 50px;
 }
 
-.search-icon {
-  margin-right: 20px;
+.night-icon {
+  width: 40px;
+  margin-left: 10px;
+  margin-right: 10px;
   color: black;
   font-size: 25px;
   cursor: pointer;
 }
 
-.search-active {
-  color: #00a182;
-}
-
-.search-icon:hover {
+.night-icon:hover {
   color: #00a182;
 }
 
@@ -170,71 +154,6 @@ export default {
 
 .list-menu a.active {
   color: #00a182;
-}
-
-.search-container {
-  padding: 20px;
-}
-
-.search-container input {
-  width: 100%;
-  max-width: 300px;
-  height: 30px;
-  margin-left: 40px;
-  padding: 5px;
-  border: 0;
-  border-bottom: 1px solid #000;
-  background-color: transparent;
-  color: #000;
-  outline: none;
-}
-
-.no-items-text {
-  margin-top: 40px;
-  font-size: 14px;
-  color: #000;
-  text-align: center;
-}
-
-.results-container {
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  flex-wrap: nowrap;
-  width: 100%;
-  height: auto;
-  margin-top: 30px;
-  padding: 0px 40px;
-}
-
-.result-item {
-  width: 90px;
-  height: 110px;
-  padding: 5px;
-  border-radius: 5px;
-  box-shadow: 0 1px 10px rgba(0, 0, 0, 0.08);
-  margin-right: 10px;
-  text-decoration: none;
-  color: #000;
-}
-
-.result-item:hover {
-  color: #00a182;
-  transition: all 0.3s ease;
-}
-
-.result-item-img {
-  width: 90px;
-  height: 80px;
-  background-color: #858585;
-  border-radius: 5px;
-}
-
-.result-item-name {
-  font-size: 14px;
-  text-align: center;
-  margin-top: 5px;
-  text-decoration: none;
 }
 
 /*ANIMACIÓN MENU*/
@@ -294,4 +213,37 @@ export default {
   transition: bottom 300ms cubic-bezier(0.23, 1, 0.32, 1), transform 300ms 300ms cubic-bezier(0.23, 1, 0.32, 1);
 }
 /*FIN ANIMACIÓN MENU*/
+
+/* Mobile media query */
+@media (max-width: 768px) {
+  .menu-container {
+    height: 30px;
+  }
+
+  .bar:before {
+    bottom: 8px;
+  }
+
+  .bar:after {
+    top: 8px;
+  }
+
+  .night-icon {
+    font-size: 23px;
+  }
+
+  .list-menu {
+    flex-direction: column;
+    height: 200px;
+  }
+
+  .list-menu a {
+    font-size: 14px;
+}
+
+  .img-logo {
+    width: 40px;
+    height: 40px;
+  }
+}
 </style>
