20b7186299162635146717ad5f86211a88b1668c
David Caballero Calvo <david.caballero94@hotmail.com>
2025-04-06 05:08:58 +0200

    Margin top mobile videos fixed

--- pages/videos.vue
+++ pages/videos.vue
@@ -84,4 +84,10 @@ export default {
   border-radius: 10px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
 }
+
+@media (max-width: 768px) {
+  .video-page-container {
+    margin-top: 50px;
+  }
+}
 </style>
