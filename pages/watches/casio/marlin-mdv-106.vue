6937d3cafafc83d6c94e59c31653c70368fe7678
David Caballero Calvo <david.caballero94@hotmail.com>
2025-04-14 02:10:42 +0200

    Post maquetado

--- pages/watches/casio/marlin-mdv-106.vue
+++ pages/watches/casio/marlin-mdv-106.vue
@@ -1,3 +1,214 @@
 <template>
-  CASIO MARLIN MDV-106
+  <div class="post-container">
+    <h1 class="post-title">Casio Marlin Duro MDV-106, adiós al pez vela</h1>
+    <p class="post-subtitle">
+      El Casio Marlin Duro MDV-106 es un reloj de buceo que ha ganado popularidad por su diseño atractivo y funcionalidad. Con una resistencia al agua de hasta 200 metros, este reloj es ideal para actividades acuáticas y deportes extremos. Su movimiento automático garantiza precisión y fiabilidad, mientras que su diseño elegante lo convierte en una opción versátil para el uso diario. En esta reseña, exploraremos las características y especificaciones del Casio Marlin Duro MDV-106, así como su rendimiento en diversas situaciones.
+    </p>
+    <div class="post-video-container">
+      <iframe class="post-video"
+            src="https://www.youtube.com/embed/MUTM5hP2InU?si=orb1AVe0Nl36XN51"
+            title="Casio Marlin Duro MDV-106, adiós al pez vela | Review en español"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+    <div class="link-container">
+      <img src="../../../public/assets/img/icons/internet.png" alt="Miravia link" class="link-icon">
+      <a href="https://s.miravia.es/s.Z9VmQ">Click aquí para comprarlo en Miravia</a>
+    </div>
+    <p class="post-paragraph">
+      El Casio Marlin Duro MDV-106 es un reloj de buceo que ha ganado popularidad por su diseño atractivo y funcionalidad. Con una resistencia al agua de hasta 200 metros, este reloj es ideal para actividades acuáticas y deportes extremos. Su movimiento automático garantiza precisión y fiabilidad, mientras que su diseño elegante lo convierte en una opción versátil para el uso diario. En esta reseña, exploraremos las características y especificaciones del Casio Marlin Duro MDV-106, así como su rendimiento en diversas situaciones.
+    </p>
+    <h2 class="post-header">Calibre</h2>
+    <p class="post-paragraph">
+      El Casio Marlin Duro MDV-106 está equipado con el movimiento automático Calibre 7S26, que es conocido por su precisión y fiabilidad. Este movimiento tiene una reserva de marcha de aproximadamente 40 horas y cuenta con una frecuencia de 21,600 alternancias por hora. Además, el Calibre 7S26 es un movimiento robusto y duradero, lo que lo convierte en una excelente opción para un reloj de buceo.
+    </p>
+    <div class="post-image-container">
+      <img src="../../../public/assets/img/watches/casio/marlin.avif" alt="Calibre Casio Marlin Duro MDV-106" class="post-image">
+    </div>
+    <h2 class="post-header">Materiales</h2>
+    <p class="post-paragraph">
+      El Casio Marlin Duro MDV-106 está fabricado con materiales de alta calidad que garantizan su durabilidad y resistencia. La caja está hecha de acero inoxidable, lo que le proporciona una gran resistencia a la corrosión y al desgaste. El cristal mineral es resistente a los arañazos y protege la esfera del reloj. Además, la correa de resina es cómoda y ligera, lo que hace que el reloj sea ideal para el uso diario.
+    </p>
+    <h2 class="post-header">Correa</h2>
+    <p class="post-paragraph">
+      La correa del Casio Marlin Duro MDV-106 está hecha de resina, lo que la hace ligera y cómoda de llevar. La correa es ajustable y cuenta con un cierre de hebilla que garantiza un ajuste seguro en la muñeca. Además, la correa es resistente al agua y a los impactos, lo que la convierte en una excelente opción para actividades acuáticas y deportes extremos.
+    </p>
+    <h2 class="post-header">Caja</h2>
+    <p class="post-paragraph">
+      La caja del Casio Marlin Duro MDV-106 está hecha de acero inoxidable, lo que le proporciona una gran resistencia a la corrosión y al desgaste. La caja tiene un diámetro de 44 mm y un grosor de 12 mm, lo que la hace adecuada para muñecas de diferentes tamaños. Además, la caja cuenta con un bisel giratorio unidireccional que permite medir el tiempo de inmersión de manera precisa.
+    </p>
+    <h2 class="post-header">Corona</h2>
+    <p class="post-paragraph">
+      La corona del Casio Marlin Duro MDV-106 está situada a las 4 en punto y es de rosca, lo que garantiza una mayor resistencia al agua. La corona es fácil de usar y permite ajustar la hora y la fecha de manera sencilla. Además, la corona cuenta con un diseño ergonómico que facilita su manipulación incluso con guantes.
+    </p>
+    <h2 class="post-header">Bisel</h2>
+    <p class="post-paragraph">
+      El bisel del Casio Marlin Duro MDV-106 es giratorio unidireccional y está marcado con una escala de 60 minutos. Este bisel permite medir el tiempo de inmersión de manera precisa y es fácil de usar incluso bajo el agua. Además, el bisel cuenta con un acabado mate que reduce los reflejos y mejora la legibilidad.
+    </p>
+    <h2 class="post-header">Dial</h2>
+    <p class="post-paragraph">
+      El bisel del Casio Marlin Duro MDV-106 es giratorio unidireccional y está marcado con una escala de 60 minutos. Este bisel permite medir el tiempo de inmersión de manera precisa y es fácil de usar incluso bajo el agua. Además, el bisel cuenta con un acabado mate que reduce los reflejos y mejora la legibilidad.
+    </p>
+    <h2 class="post-header">Marcadores</h2>
+    <p class="post-paragraph">
+      Los marcadores del Casio Marlin Duro MDV-106 son grandes y luminosos, lo que facilita la lectura de la hora en condiciones de poca luz. Los índices están aplicados en la esfera y cuentan con un acabado mate que reduce los reflejos. Además, las manecillas del reloj están recubiertas con material luminiscente, lo que mejora la visibilidad en la oscuridad.
+    </p>
+    <h2 class="post-header">Fechador</h2>
+    <p class="post-paragraph">
+      El Casio Marlin Duro MDV-106 cuenta con un fechador situado a las 3 en punto. Este fechador es fácil de leer y se ajusta automáticamente al final de cada mes. Además, el fechador cuenta con un fondo blanco que mejora la legibilidad y facilita la lectura de la fecha.
+    </p>
+    <h2 class="post-header">Lumen</h2>
+    <p class="post-paragraph">
+      El Casio Marlin Duro MDV-106 cuenta con un lumen de alta calidad que garantiza una excelente visibilidad en condiciones de poca luz. El lumen es brillante y duradero, lo que permite leer la hora con facilidad incluso en la oscuridad. Además, el lumen se recarga rápidamente con la luz solar o artificial, lo que garantiza una visibilidad constante.
+    </p>
+    <h2 class="post-header">Cristal</h2>
+    <p class="post-paragraph">
+      El cristal del Casio Marlin Duro MDV-106 es de mineral, lo que le proporciona una gran resistencia a los arañazos y al desgaste. Este cristal es transparente y permite una excelente visibilidad de la esfera del reloj. Además, el cristal cuenta con un tratamiento antirreflejos que reduce los reflejos y mejora la legibilidad en condiciones de luz intensa.
+    </p>
+    <h2 class="post-header">Tapa</h2>
+    <p class="post-paragraph">
+      La tapa del Casio Marlin Duro MDV-106 está hecha de acero inoxidable y cuenta con un acabado mate que reduce los reflejos. Esta tapa es resistente a la corrosión y al desgaste, lo que garantiza una larga vida útil del reloj. Además, la tapa cuenta con un diseño ergonómico que facilita su manipulación y apertura.
+    </p>
+    <h2 class="post-header">Medidas</h2>
+    <p class="post-paragraph">
+      El Casio Marlin Duro MDV-106 tiene un diámetro de 44 mm y un grosor de 12 mm, lo que lo hace adecuado para muñecas de diferentes tamaños. Además, el reloj cuenta con una resistencia al agua de hasta 200 metros, lo que lo convierte en una excelente opción para actividades acuáticas y deportes extremos. El peso del reloj es de aproximadamente 150 gramos, lo que lo hace ligero y cómodo de llevar.
+    </p>
+    <h2 class="post-header">Precio</h2>
+    <p class="post-paragraph">
+      El precio del Casio Marlin Duro MDV-106 varía según el lugar de compra y las ofertas disponibles. En general, este reloj se encuentra en un rango de precios asequible, lo que lo convierte en una excelente opción para aquellos que buscan un reloj de buceo de calidad sin gastar una fortuna. En Miravia, puedes encontrarlo por menos de 100 euros.
+    </p>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'marlin-mdv-106',
+  head: {
+    title: 'Casio Marlin Duro MDV-106, adiós al pez vela - Tiempo de Calidad',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Relojería, precisión y calidad en cada review'
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/icon.ico' }]
+  }
+}
+</script>
+
+<style scoped>
+.post-container {
+  max-width: 750px;
+  margin: 0 auto;
+  padding: 20px;
+  margin-top: 100px;
+}
+
+.post-title {
+  font-size: 32px;
+  font-weight: bold;
+  margin-bottom: 40px;
+  text-align: center;
+}
+
+.post-subtitle {
+  font-size: 16px;
+  font-weight: 300;
+  text-align: justify;
+  margin-bottom: 40px;
+}
+
+.post-video-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.post-video {
+  width: 600px;
+  height: 360px;
+  border-radius: 8px;
+}
+
+.link-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+  margin-bottom: 50px;
+}
+
+.link-icon {
+  width: 30px;
+  height: 30px;
+  margin-right: 10px;
+}
+
+.link-container a {
+  text-decoration: none;
+  font-size: 16px;
+  font-weight: bold;
+}
+
+.dark-mode .link-container a {
+  color: #ffffff;
+}
+
+.light-mode .link-container a {
+  color: #000000;
+}
+
+.link-container a:hover {
+  color: #00a182;
+}
+
+.post-header {
+  font-size: 24px;
+  font-weight: bold;
+  margin-top: 40px;
+  margin-bottom: 20px;
+}
+
+.post-paragraph {
+  font-size: 16px;
+  font-weight: 300;
+  text-align: justify;
+}
+
+.post-image-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+.post-image {
+  width: 100%;
+  max-width: 500px;
+  height: auto;
+  border-radius: 8px;
+}
+
+@media (max-width: 768px) {
+  .post-container {
+    padding: 10px;
+    margin-top: 80px;
+  }
+
+  .post-title {
+    font-size: 28px;
+    margin-bottom: 30px;
+  }
+
+  .post-video {
+    width: 300px;
+    height: 169px;
+  }
+}
+</style>
