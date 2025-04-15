137c9a0b48753de3d59f6249cdefb947487261b1
DavidCaba6194 <cabayosoyasi@gmail.com>
2025-04-15 16:41:12 +0200

    Nuevo post Casio Marlin

--- pages/watches/casio/marlin-mdv-106.vue
+++ pages/watches/casio/marlin-mdv-106.vue
@@ -2,12 +2,12 @@
   <div class="post-container">
     <Head>
       <Title>Casio Marlin Duro MDV-106, adiós al pez vela - Tiempo de Calidad</Title>
-      <Meta name="description" content="El Casio Marlin Duro MDV-106 es un reloj de buceo que ha ganado popularidad por su diseño atractivo y funcionalidad. Con una resistencia al agua de hasta 200 metros, este reloj es ideal para actividades acuáticas y deportes extremos. Su movimiento automático garantiza precisión y fiabilidad, mientras que su diseño elegante lo convierte en una opción versátil para el uso diario. En esta reseña, exploraremos las características y especificaciones del Casio Marlin Duro MDV-106, así como su rendimiento en diversas situaciones." />
+      <Meta name="description" content="Con la fiabilidad de esta gran marca japonesa por bandera y luciendo el pez vela en su dial, estamos ante un modelo de Casio en peligro de extinción. En el año 2021 perdieron los derechos de imagen sobre este pez vela y se vieron obligados a descontinuar este modelo, presentando su sucesor exactamente igual pero sin el logo del Marlin en el dial." />
       <link rel="icon" type="image/x-icon" href="/icon.ico" />
     </Head>
     <h1 class="post-title">Casio Marlin Duro MDV-106, adiós al pez vela</h1>
     <p class="post-subtitle">
-      El Casio Marlin Duro MDV-106 es un reloj de buceo que ha ganado popularidad por su diseño atractivo y funcionalidad. Con una resistencia al agua de hasta 200 metros, este reloj es ideal para actividades acuáticas y deportes extremos. Su movimiento automático garantiza precisión y fiabilidad, mientras que su diseño elegante lo convierte en una opción versátil para el uso diario. En esta reseña, exploraremos las características y especificaciones del Casio Marlin Duro MDV-106, así como su rendimiento en diversas situaciones.
+      Con la fiabilidad de esta gran marca japonesa por bandera y luciendo el pez vela en su dial, estamos ante un modelo de Casio en peligro de extinción. En el año 2021 perdieron los derechos de imagen sobre este pez vela y se vieron obligados a descontinuar este modelo, presentando su sucesor exactamente igual pero sin el logo del Marlin en el dial.
     </p>
     <div class="post-video-container">
       <iframe class="post-video"
@@ -22,66 +22,70 @@
       <a href="https://s.miravia.es/s.Z9VmQ">Click aquí para comprarlo en Miravia</a>
     </div>
     <p class="post-paragraph">
-      El Casio Marlin Duro MDV-106 es un reloj de buceo que ha ganado popularidad por su diseño atractivo y funcionalidad. Con una resistencia al agua de hasta 200 metros, este reloj es ideal para actividades acuáticas y deportes extremos. Su movimiento automático garantiza precisión y fiabilidad, mientras que su diseño elegante lo convierte en una opción versátil para el uso diario. En esta reseña, exploraremos las características y especificaciones del Casio Marlin Duro MDV-106, así como su rendimiento en diversas situaciones.
+      Dejando la historia a un lado, podemos ver que estamos ante un reloj de tipo Diver. Esto significa que está pensado para su uso debajo del agua. Todas sus características, que iremos viendo a continuación, están adaptadas para cumplir con este propósito y dotarlo de una resistencia al agua de 200 metros.
     </p>
     <h2 class="post-header">Calibre</h2>
     <p class="post-paragraph">
-      El Casio Marlin Duro MDV-106 está equipado con el movimiento automático Calibre 7S26, que es conocido por su precisión y fiabilidad. Este movimiento tiene una reserva de marcha de aproximadamente 40 horas y cuenta con una frecuencia de 21,600 alternancias por hora. Además, el Calibre 7S26 es un movimiento robusto y duradero, lo que lo convierte en una excelente opción para un reloj de buceo.
+      Equipa un calibre de cuarzo, concretamente el conocido Miyota 2115. Sobre el papel y sobre la práctica, este calibre tiene una desviación de unos 20 segundos al mes. Esto es algo que se espera de todos los calibres de este tipo, pero está bien que lo cumpla. Además de esto, la marca promete una duración de la pila de 3 años.
     </p>
     <div class="post-image-container">
       <img src="../../../public/assets/img/watches/casio/marlin.png" alt="Calibre Casio Marlin Duro MDV-106" class="post-image">
     </div>
     <h2 class="post-header">Materiales</h2>
     <p class="post-paragraph">
-      El Casio Marlin Duro MDV-106 está fabricado con materiales de alta calidad que garantizan su durabilidad y resistencia. La caja está hecha de acero inoxidable, lo que le proporciona una gran resistencia a la corrosión y al desgaste. El cristal mineral es resistente a los arañazos y protege la esfera del reloj. Además, la correa de resina es cómoda y ligera, lo que hace que el reloj sea ideal para el uso diario.
+      En cuanto a sus materiales tenemos lo que se puede esperar en un reloj de estas características: Acero inoxidable para la caja y, en este caso, resina para la correa. En general y gracias a estos materiales, es un reloj que se siente sólido y bien construido cuando lo llevas puesto.
     </p>
     <h2 class="post-header">Correa</h2>
     <p class="post-paragraph">
-      La correa del Casio Marlin Duro MDV-106 está hecha de resina, lo que la hace ligera y cómoda de llevar. La correa es ajustable y cuenta con un cierre de hebilla que garantiza un ajuste seguro en la muñeca. Además, la correa es resistente al agua y a los impactos, lo que la convierte en una excelente opción para actividades acuáticas y deportes extremos.
+      La correa, como ya hemos comentado, es de resina negra. Es cómoda y se ajusta bien a la muñeca, pero dejando espacio para evitar el sudor. Se caracteriza por tener una buena resistencia, ya que si se usa el reloj para su propósito principal es posible que tenga que resistir a roces y golpes. La hebilla es de acero inoxidable con un acabado cepillado pero sin firmar.
     </p>
     <h2 class="post-header">Caja</h2>
     <p class="post-paragraph">
-      La caja del Casio Marlin Duro MDV-106 está hecha de acero inoxidable, lo que le proporciona una gran resistencia a la corrosión y al desgaste. La caja tiene un diámetro de 44 mm y un grosor de 12 mm, lo que la hace adecuada para muñecas de diferentes tamaños. Además, la caja cuenta con un bisel giratorio unidireccional que permite medir el tiempo de inmersión de manera precisa.
+      La caja tiene un ligero cepillado por la parte superior y un acabado pulido tanto en los laterales como en el canto que une las asas. Es una caja con formas suaves y redondeadas creando una armonía que se percibe totalmente orgánica. Un lujo para la muñeca y una gran comodidad a la hora de llevar puesto el reloj, por lo menos para mi gusto.
     </p>
     <h2 class="post-header">Corona</h2>
     <p class="post-paragraph">
-      La corona del Casio Marlin Duro MDV-106 está situada a las 4 en punto y es de rosca, lo que garantiza una mayor resistencia al agua. La corona es fácil de usar y permite ajustar la hora y la fecha de manera sencilla. Además, la corona cuenta con un diseño ergonómico que facilita su manipulación incluso con guantes.
+      Como no podía ser de otra forma, tenemos una corona roscada. Esta característica sirve para asegurar la estanqueidad del reloj a la hora de sumergirlo. Tiene un acabado pulido y está protegida por un guardacoronas que no sobresale en exceso pero que cumple perfectamente su función. Un tema de debate es la firma en la corona, detalle que yo agradezco pero que no a todo el mundo le gusta. En este caso no la lleva, dejando solamente el acabado pulido. En general es una buena corona ya que se opera con facilidad, tracciona bien y transmite perfectamente el movimiento de las agujas.
     </p>
     <h2 class="post-header">Bisel</h2>
     <p class="post-paragraph">
-      El bisel del Casio Marlin Duro MDV-106 es giratorio unidireccional y está marcado con una escala de 60 minutos. Este bisel permite medir el tiempo de inmersión de manera precisa y es fácil de usar incluso bajo el agua. Además, el bisel cuenta con un acabado mate que reduce los reflejos y mejora la legibilidad.
+      Tenemos un bisel de acero inoxidable, unidireccional y de 120 clicks. El tacto es bueno y opone suficiente resistencia al moverlo como para despreocuparse de que se mueva sin querer. Tiene pintados número arábigos en los múltiplos de 10, bastones en los acabados en 5 y un trapecio invertido con una gota de lumen en la posición de las 12. Este combina los colores azul y negro, por eso es apodado “Batman”, pero está disponible en otros distintos también. Si bien es cierto que por menos dinero hay relojes chinos que tienen bisel cerámico, no se le puede sacar ninguna pega a la calidad del de este Marlin.
     </p>
     <h2 class="post-header">Dial</h2>
     <p class="post-paragraph">
-      El bisel del Casio Marlin Duro MDV-106 es giratorio unidireccional y está marcado con una escala de 60 minutos. Este bisel permite medir el tiempo de inmersión de manera precisa y es fácil de usar incluso bajo el agua. Además, el bisel cuenta con un acabado mate que reduce los reflejos y mejora la legibilidad.
+      El dial es negro con efecto rayo de sol. Esto hace que al girarlo hacia la luz, esta refleje círculos concéntricos sobre el fondo negro dle dial. Alrededor del mismo nos encontramos un fino riot que contiene los marcadores de los minutos pintados en blanco sobre fondo negor. Debajo de las 12 el logo de la marca y encima de las 6 el logo del Marlin y la resistencia al agua de 200 metros, todos ellos pintados en blanco también.
     </p>
     <h2 class="post-header">Marcadores</h2>
     <p class="post-paragraph">
-      Los marcadores del Casio Marlin Duro MDV-106 son grandes y luminosos, lo que facilita la lectura de la hora en condiciones de poca luz. Los índices están aplicados en la esfera y cuentan con un acabado mate que reduce los reflejos. Además, las manecillas del reloj están recubiertas con material luminiscente, lo que mejora la visibilidad en la oscuridad.
+      Los marcadores son aplicados con forma rectangular a las 3, 6 y 9, doble trapecio a las 12 y círculos para el resto de horas. Todos tienen un fino marco cromado alrededor de ellos que hace que resalte su relieve por encima del dial. En su interior están pintados de pasta luminosa que luego analizaremos con detalle.
     </p>
     <h2 class="post-header">Fechador</h2>
     <p class="post-paragraph">
-      El Casio Marlin Duro MDV-106 cuenta con un fechador situado a las 3 en punto. Este fechador es fácil de leer y se ajusta automáticamente al final de cada mes. Además, el fechador cuenta con un fondo blanco que mejora la legibilidad y facilita la lectura de la fecha.
+      El fechador se encuentra a las 3, integrado en el espacio que le cede el marcador rectangular que hay en esa posición. Está enmarcado por una línea blanca y cortado en ángulo a modo de paspartú. Sobre fondo blanco, pintados en negro, tenemos los indicadores del día del mes en números arábigos. En mi opinión me hubiera gustado más que el fechador estuviera en negativo, es decir con números blancos sobre fondo negro.
+    </p>
+    <h2 class="post-header">Manecillas</h2>
+    <p class="post-paragraph">
+      Este Casio cuenta con tres manecillas centrales, todas ellas contrapesadas y rellenas de lumen. La horaria más corta y con forma de flecha, la minutera con forma de espada y la trotadora con forma de dardo en la punta. Un detalle de esta última es que está pintada completamente en azul, haciendo juego con el color del bisel de este modelo.
     </p>
     <h2 class="post-header">Lumen</h2>
     <p class="post-paragraph">
-      El Casio Marlin Duro MDV-106 cuenta con un lumen de alta calidad que garantiza una excelente visibilidad en condiciones de poca luz. El lumen es brillante y duradero, lo que permite leer la hora con facilidad incluso en la oscuridad. Además, el lumen se recarga rápidamente con la luz solar o artificial, lo que garantiza una visibilidad constante.
+      El lumen es lo que peor sabor de boca me deja de este reloj. Esperaba que siendo un diver, por lo menos, aguantase varias horas o incluso con más intensidad. Tampoco es algo para echarse las manos a la cabeza, pero es una pena que haya marcas que lo superen en este aspecto aún siendo más baratas. De todas formas, con todo lo que ofrece y siendo el reloj que es, se lo podremos perdonar.
     </p>
     <h2 class="post-header">Cristal</h2>
     <p class="post-paragraph">
-      El cristal del Casio Marlin Duro MDV-106 es de mineral, lo que le proporciona una gran resistencia a los arañazos y al desgaste. Este cristal es transparente y permite una excelente visibilidad de la esfera del reloj. Además, el cristal cuenta con un tratamiento antirreflejos que reduce los reflejos y mejora la legibilidad en condiciones de luz intensa.
+      Con el cristal nos encontramos un poco en la misma tesitura. Es un buen cristal, pero es mineral. Volvemos de nuevo a la competencia con cristal de zafiro en este mismo rango de precio, pero ya sabemos que contra los chinos poco se puede hacer. Sobresale apenas un milímetro por encima del bisel y queda perfectamente enmarcado en el mismo.
     </p>
     <h2 class="post-header">Tapa</h2>
     <p class="post-paragraph">
-      La tapa del Casio Marlin Duro MDV-106 está hecha de acero inoxidable y cuenta con un acabado mate que reduce los reflejos. Esta tapa es resistente a la corrosión y al desgaste, lo que garantiza una larga vida útil del reloj. Además, la tapa cuenta con un diseño ergonómico que facilita su manipulación y apertura.
+      Y llegamos a la tapa, que es roscada y de acero inoxidable. Tiene un acabado cepillado haciendo círculos concéntricos y varias leyendas grabadas. Entre ellas, formando un anillo, tenemos el material, la resistencia al agua y el modelo del reloj. En horizontal vemos la marca y la procedencia del reloj. En el medio, presidiendo, tenemos el Marlin o pez vela que caracteriza a este modelo en concreto. Una curiosidad es que en cada modelo el pez viene en una posición distinta, supongo que por la dejadez de Casio de no alinear perfectamente todas las tapas.
     </p>
     <h2 class="post-header">Medidas</h2>
     <p class="post-paragraph">
-      El Casio Marlin Duro MDV-106 tiene un diámetro de 44 mm y un grosor de 12 mm, lo que lo hace adecuado para muñecas de diferentes tamaños. Además, el reloj cuenta con una resistencia al agua de hasta 200 metros, lo que lo convierte en una excelente opción para actividades acuáticas y deportes extremos. El peso del reloj es de aproximadamente 150 gramos, lo que lo hace ligero y cómodo de llevar.
+      Estamos ante un reloj grande, pero es que todos los buceadores lo son por norma general. Este Marlin tiene 44.2 mm de diámetro, 48.5 mm de largo entre postes y 12.1 mm de grosor. Unas medidas que no son para cualquier muñeca, aunque he de decir que una vez puesto no parece tan grande como es.
     </p>
     <h2 class="post-header">Precio</h2>
     <p class="post-paragraph">
-      El precio del Casio Marlin Duro MDV-106 varía según el lugar de compra y las ofertas disponibles. En general, este reloj se encuentra en un rango de precios asequible, lo que lo convierte en una excelente opción para aquellos que buscan un reloj de buceo de calidad sin gastar una fortuna. En Miravia, puedes encontrarlo por menos de 100 euros.
+      Aquí llegamos a terreno pantanoso. La descontinuación de este modelo ha hecho que se dispare de precio más de lo normal, teniendo en cuenta que su sucesor es exactamente igual y más barato. Pero ya sabemos como funciona esto de la escasez y la exclusividad. No es raro encontrarte ofertas de este Marlin por encima de los 120 euros, cuando su precio original era casi la mitad. Yo lo pude conseguir en el enlace de Miravia que tenéis arriba por 75 euros con descuento. Si lo encontráis por un precio similar me parece una muy buena compra, aunque si tenéis que pagar más yo creo que también merece la pena por tener un trozo de historia en tu colección.
     </p>
   </div>
 </template>
@@ -97,7 +101,7 @@ export default {
       {
         hid: 'description',
         name: 'description',
-        content: 'El Casio Marlin Duro MDV-106 es un reloj de buceo que ha ganado popularidad por su diseño atractivo y funcionalidad. Con una resistencia al agua de hasta 200 metros, este reloj es ideal para actividades acuáticas y deportes extremos. Su movimiento automático garantiza precisión y fiabilidad, mientras que su diseño elegante lo convierte en una opción versátil para el uso diario. En esta reseña, exploraremos las características y especificaciones del Casio Marlin Duro MDV-106, así como su rendimiento en diversas situaciones.'
+        content: 'Con la fiabilidad de esta gran marca japonesa por bandera y luciendo el pez vela en su dial, estamos ante un modelo de Casio en peligro de extinción. En el año 2021 perdieron los derechos de imagen sobre este pez vela y se vieron obligados a descontinuar este modelo, presentando su sucesor exactamente igual pero sin el logo del Marlin en el dial.'
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/icon.ico' }]
