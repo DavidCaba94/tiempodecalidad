1ae16ae8feabd1cb19c74132b8116351afdca924
David Caballero Calvo <david.caballero94@hotmail.com>
2025-05-19 19:44:39 +0200

    Nuevo post Seiko falso

---
+++ pages/reviews/seiko-falso-aliexpress.vue
@@ -0,0 +1,231 @@
+<template>
+  <div class="post-container">
+    <Head>
+      <Title>Falsificaciones Seiko en Aliexpress, ¿merecen la pena? - Tiempo de Calidad</Title>
+      <Meta name="description" content="En el apasionante mundo de la relojería, hay algo que todos compartimos: la tentación. Y si navegas por AliExpress con cierta frecuencia, probablemente te habrás topado con relojes que, a primera vista, parecen auténticas gangas. Marcas reconocidas, diseños icónicos… y precios que rozan el absurdo. ¿Un Seiko Prospex por 20 euros? Suena demasiado bien para ser cierto, y ¿lo es?." />
+      <link rel="icon" type="image/x-icon" href="/icon.ico" />
+    </Head>
+    <h1 class="post-title">Falsificaciones Seiko en Aliexpress, ¿merecen la pena?</h1>
+    <p class="post-subtitle">
+      En el apasionante mundo de la relojería, hay algo que todos compartimos: la tentación. Y si navegas por AliExpress con cierta frecuencia, probablemente te habrás topado con relojes que, a primera vista, parecen auténticas gangas. Marcas reconocidas, diseños icónicos… y precios que rozan el absurdo. ¿Un Seiko Prospex por 20 euros? Suena demasiado bien para ser cierto, y ¿lo es?.
+    </p>
+    <div class="post-video-container">
+      <iframe class="post-video"
+            src="https://www.youtube.com/embed/n7RVX0Nkr5Q?si=JV6fWv82CCBP1wxy"
+            title="Compro un Seiko falso en Aliexpress por la ciencia | Review en español"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+    <p class="post-paragraph">
+      En este artículo vamos a hablar de las falsificaciones que inundan plataformas como AliExpress, y para analizarlo en profundidad me he aventurado a comprar una de estas imitaciones con fines estrictamente científicos. A continuación, te presento la reseña detallada de este espectacular clon de 20 euros que imita a un Seiko Prospex Speedtimer. Spoiler: ni se le acerca.
+    </p>
+    <h2 class="post-header">La trampa está servida</h2>
+    <p class="post-paragraph">
+      No es raro encontrarse en AliExpress con relojes de marcas conocidas a precios muy por debajo de su valor de mercado. Un ejemplo reciente: un <strong>Citizen Eco-Drive por apenas 27 euros</strong>, diez veces menos que en la tienda oficial. Para los que conocen el modelo, la falsedad salta a la vista. Pero alguien que acaba de aterrizar en este mundo puede caer en la trampa y terminar con un reloj de pésima calidad y un vendedor imposible de localizar.
+    </p>
+    <p class="post-paragraph">
+      Otro caso es un <strong>Seiko 5</strong> vendido a una décima parte de su precio original, con sorprendentemente buenas valoraciones. ¿Casualidad? ¿Usuarios desinformados? ¿Opiniones compradas? El reloj, a simple vista, dista mucho del original, y por ese mismo precio hay opciones auténticas como ciertos modelos de Casio que ofrecen mucha más calidad.
+    </p>
+    <h2 class="post-header">Reseña completa: “Seiko” Prospex Speedtimer de 20 euros</h2>
+    <p class="post-paragraph">
+      Pasamos ahora al protagonista de esta historia: una copia descarada del Seiko Prospex Speedtimer. Lo compré por unos 20 euros, y como era de esperar, el vendedor desapareció poco después del envío. Ya que me lo voy a tener que comer con patatas, al menos voy a sacarle partido con esta reseña.
+    </p>
+    <h2 class="post-header">Calibre</h2>
+    <p class="post-paragraph">
+      Lo único que indica el vendedor es que es un <strong>movimiento de cuarzo</strong>, sin especificar calibre ni procedencia. Nada sorprendente. No esperaba un informe técnico, pero al menos una referencia habría sido útil para investigar algo más. A falta de información, lo único que se puede hacer es asumir que se trata de uno de los movimientos genéricos más baratos disponibles en el mercado.
+    </p>
+    <h2 class="post-header">Correa</h2>
+    <p class="post-paragraph">
+      Según el vendedor, el brazalete es de acero inoxidable. La realidad es otra: tiene toda la pinta de ser <strong>chapa o latón</strong>, y el sonido a sonajero al moverlo lo confirma. Los eslabones están huecos y muestran pliegues visibles. El cierre es deplorable: se libera por presión, se cierra solo si le apetece, y tiene tanto juego como un cajón desajustado. Supuestamente ofrece dos posiciones de microajuste… aunque parece que una de ellas es decorativa.
+    </p>
+    <h2 class="post-header">Caja</h2>
+    <p class="post-paragraph">
+      La caja está anunciada como acero inoxidable, y aunque <strong>no hay forma de comprobar la calidad del acero</strong>, al menos parece metálica. Está pulida a espejo y guarda cierta semejanza con el original en líneas generales. Sin embargo, el ajuste entre caja y brazalete deja rendijas visibles, lo que arruina el conjunto.
+    </p>
+    <div class="post-image-container">
+      <img src="../../public/assets/img/reviews/seiko-falso-aliexpress/seiko-falso-aliexpress.png" alt="Falsificación Seiko Prospex Speedtimer" class="post-image">
+    </div>
+    <h2 class="post-header">Corona y pulsadores</h2>
+    <p class="post-paragraph">
+      La <strong>corona</strong> es probablemente el componente mejor logrado del reloj. Tiene un <strong>tacto decente</strong>, buena tracción y las posiciones del fechador y ajuste de hora están bien definidas. Eso sí, el diseño no coincide con el del modelo original. En cuanto a <strong>los pulsadores del cronógrafo, son todo lo contrario</strong>: mal tacto, sin sensación de precisión, y parecen más decorativos que funcionales.
+    </p>
+    <h2 class="post-header">Bisel</h2>
+    <p class="post-paragraph">
+      Fabricado en un material metálico no especificado, el bisel tiene buen encaje con la caja y está cortado en ángulo de 45º, con unión a 90º. <strong>La parte negativa son los marcadores del taquímetro</strong>, que están en bajo relieve y pintados con una tinta blanca de tan mala calidad que los números son ilegibles en varias zonas.
+    </p>
+    <h2 class="post-header">Dial</h2>
+    <p class="post-paragraph">
+      El dial <strong>blanco mate</strong> presenta tres subesferas: minutos del crono a la izquierda, segundero en la parte inferior y formato 24h a la derecha. Encima de las 12 aparece el logotipo de Seiko junto a la X de Prospex, todo pintado. Los <strong>índices aplicados</strong> son rectangulares y pulidos, aunque los de las posiciones 3, 6 y 9 son más cortos para no solapar los subdiales.
+    </p>
+    <p class="post-paragraph">
+      En el modelo original, los índices principales tienen <strong>rectángulos de lumen</strong>, algo ausente en esta versión. El fechador está colocado entre las 4 y las 5, sin marco, pero <strong>los números aparecen descuadrados</strong>, lo que estropea el conjunto visual.
+    </p>
+    <h2 class="post-header">Agujas</h2>
+    <p class="post-paragraph">
+      Las agujas son <strong>de lo poco aceptable</strong>. Minutera y horaria tienen forma de lápiz, contorno negro e interior con lume. La del cronógrafo es tipo pica y también cuenta con lume en la punta. Todas están <strong>ligeramente contrapesadas</strong>, como en el modelo original.
+    </p>
+    <h2 class="post-header">Lúmen</h2>
+    <p class="post-paragraph">
+      Aquí no hay sorpresas. <strong>Solo brillan las tres agujas principales</strong>, y el efecto desaparece tras dos minutos. Nada que ver con el lume real de un Seiko Prospex.
+    </p>
+    <h2 class="post-header">Cristal</h2>
+    <p class="post-paragraph">
+      El cristal es <strong>mineral</strong>, sobresale bastante del bisel y, como era de esperar, no tiene tratamiento antirreflejante. Dudo que sobreviva a arañazos o caídas, aunque sinceramente no tengo intención de comprobarlo: este reloj no volverá a ver la luz después de esto.
+    </p>
+    <h2 class="post-header">Tapa trasera</h2>
+    <p class="post-paragraph">
+      Es una <strong>tapa a presión</strong>, pulida a espejo, con las típicas inscripciones genéricas sobre el material y la resistencia al agua. Curiosamente, no hay rastro del “30M Water Resistant” que dice el vendedor, lo que me lleva a pensar que una simple gota puede ser su sentencia.
+    </p>
+    <h2 class="post-header">Medidas y peso</h2>
+    <p class="post-paragraph">
+      En cuanto a dimensiones, el reloj mantiene unas proporciones bastante estándar para un cronógrafo moderno, aunque algo compactas si lo comparamos con el modelo original: <strong>Largo (de asa a asa):</strong> 45 mm, <strong>Diámetro de la caja:</strong> 39 mm, <strong>Grosor:</strong> 13,3 mm
+    </p>
+    <p class="post-paragraph">
+      A simple vista, estas cifras no están mal, pero en la muñeca <strong>se siente más liviano de lo esperado</strong>. El motivo es evidente: los materiales. Aunque no se especifica el peso en la ficha del producto, calculo que <strong>rondará los 80 gramos</strong>, lo que confirma que estamos ante un conjunto de componentes más cercano a la chapa decorativa que a un reloj de verdad.
+    </p>
+    <p class="post-paragraph">
+      El resultado es un reloj que puede parecer compacto y cómodo, pero que <strong>transmite muy poca solidez</strong>. Ese “peso pluma” resta toda sensación de calidad y refuerza la impresión general de estar ante un <strong>producto barato y mal construido</strong>.
+    </p>
+    <h2 class="post-header">Conclusión</h2>
+    <p class="post-paragraph">
+      Esta vez no hablaré de si el precio justifica la compra, porque <strong>ni por 20 euros recomendaría alimentar este tipo de mercado</strong>. Yo lo compré con fines educativos, pero dudo que pueda devolverlo. Si te gusta un modelo concreto pero no puedes permitirte el original, existen homenajes de marcas fiables que ofrecen buena calidad, calibres decentes y diseño fiel, sin caer en la falsificación descarada.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'seiko-falso-aliexpress',
+  head: {
+    title: 'Falsificaciones Seiko en Aliexpress, ¿merecen la pena? - Tiempo de Calidad',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'En el apasionante mundo de la relojería, hay algo que todos compartimos: la tentación. Y si navegas por AliExpress con cierta frecuencia, probablemente te habrás topado con relojes que, a primera vista, parecen auténticas gangas. Marcas reconocidas, diseños icónicos… y precios que rozan el absurdo. ¿Un Seiko Prospex por 20 euros? Suena demasiado bien para ser cierto, y ¿lo es?.'
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/icon.ico' }]
+  }
+}
+</script>
+
+<style scoped>
+.post-container {
+  max-width: 750px;
+  margin: 0 auto;
+  padding: 20px;
+  margin-top: 100px;
+}
+
+strong {
+  font-weight: bold;
+}
+
+.post-title {
+  font-size: 32px;
+  font-weight: bold;
+  margin-bottom: 40px;
+  text-align: center;
+}
+
+.post-subtitle {
+  font-size: 16px;
+  font-weight: 300;
+  text-align: justify;
+  margin-bottom: 40px;
+}
+
+.post-video-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.post-video {
+  width: 600px;
+  height: 335px;
+  border-radius: 8px;
+}
+
+.link-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+  margin-bottom: 50px;
+}
+
+.link-icon {
+  width: 30px;
+  height: 30px;
+  margin-right: 10px;
+}
+
+.link-container a {
+  text-decoration: none;
+  font-size: 16px;
+  font-weight: bold;
+}
+
+.dark-mode .link-container a {
+  color: #ffffff;
+}
+
+.light-mode .link-container a {
+  color: #000000;
+}
+
+.link-container a:hover {
+  color: #00a182;
+}
+
+.post-header {
+  font-size: 24px;
+  font-weight: bold;
+  margin-top: 40px;
+  margin-bottom: 20px;
+}
+
+.post-paragraph {
+  font-size: 16px;
+  font-weight: 300;
+  text-align: justify;
+}
+
+.post-image-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+.post-image {
+  width: 100%;
+  max-width: 500px;
+  height: auto;
+  border-radius: 8px;
+}
+
+@media (max-width: 768px) {
+  .post-container {
+    padding: 10px;
+    margin-top: 80px;
+  }
+
+  .post-title {
+    font-size: 28px;
+    margin-bottom: 30px;
+  }
+
+  .post-video {
+    width: 300px;
+    height: 169px;
+  }
+}
+</style>
