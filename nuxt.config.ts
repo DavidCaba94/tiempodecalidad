8978395a58d94ffe68300de5e811b640aa75f7cc
DavidCaba6194 <cabayosoyasi@gmail.com>
2025-10-01 18:19:00 +0200

    Config y packages actualizados

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,21 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-03-28',
+  compatibilityDate: '2025-10-01',
   devtools: { enabled: true },
   modules: [
     '@nuxt/fonts',
     '@nuxt/icon',
     '@nuxt/image',
     '@nuxt/scripts',
-    '@nuxtjs/sitemap'
+    '@nuxtjs/robots',
+    'nuxt-simple-sitemap'
   ],
   site: { 
     url: 'https://tiempodecalidad.net', 
     name: 'Tiempo de Calidad' 
+  },
+  robots: {
+    allow: ['/'],
+    disallow: ['/admin', '/login'],
+    sitemap: 'https://tiempodecalidad.net/sitemap.xml'
   }
 })
